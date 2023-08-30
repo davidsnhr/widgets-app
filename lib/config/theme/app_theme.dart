44a0534500e4394c2268b9ffab0723a8f188cda6
David Sánchez <davidsanchez@Davids-MacBook-Pro-2.local>
2023-08-29 21:46:08 -0600

    add home screen scaffold

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -29,6 +29,9 @@ class AppTheme {
 
   ThemeData getTheme() => ThemeData(
     useMaterial3: true,
-    colorSchemeSeed: colorList[selectedColor]
+    colorSchemeSeed: colorList[selectedColor],
+    appBarTheme: const AppBarTheme(
+      centerTitle: false
+    )
   );
 }
